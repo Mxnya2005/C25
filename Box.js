78670c565caf91ac807e19bfb0d19d38b86c22ec
Mxnya2005 <71551721+Mxnya2005@users.noreply.github.com>
2020-11-01 13:32:07 +0530

    Add files via upload

--- Box.js
+++ Box.js
@@ -1,28 +1,7 @@
-class Box{
-    constructor(x,y,width,height)
-    {
-        var options={
-            restitution:0.5,
-         friction:2   
-        }
-
-        this.body = Bodies.rectangle(x,y,width,height,options);
-        this.width = width;
-        this.height = height;
-        World.add(gameworld,this.body);
-    }
-
-    display()
-    {
-        var angle= this.body.angle
-        push ();
-        translate(this.body.position.x,this.body.position.y)
-        rotate (angle)
-        strokeWeight(3)
-        stroke("orange");
-        fill ("cyan")
-        rectMode(CENTER);
-        rect(0,0,this.width,this.height)
-        pop (); 
-    }
-}
+class Box extends BaseClass {
+  constructor(x, y, width, height) {
+    
+    super(x,y,width,height)
+    this.image=loadImage ("sprites/wood1.png")
+  }
+};
--- sketch.js
+++ sketch.js
@@ -1,58 +1,62 @@
 const Engine = Matter.Engine;
-const World = Matter.World;
+const World= Matter.World;
 const Bodies = Matter.Bodies;
 
-var gameengine,gameworld;
-var ground;
-var box1,box2,box3,box4,box5;
-var Pig1;
-var Pig2;
-var Log1;
-var Log2;
-var Log3;
-var Log4;
-var bird;
+var engine, world;
+var box1, pig1;
+var backgroundImage;
 
-function setup() {
-  createCanvas(1200,400);
-
-  gameengine = Engine.create();
-  gameworld = gameengine.world;
-
-  ground= new Ground(600,390,1200,20);
-
-  box1 = new Box(700,320,70,70);
-  box2 = new Box(920,320,70,70);
-  Pig1 = new Pig(810,350);
- Log1 = new Log(810,250,300,PI/2)
-
-box3 = new Box(700,230,70,70);
-box4 = new Box(920,230,70,70);
-Pig2 = new Pig(810,210);
-Log2 = new Log(810,170,300,PI/2);
-
-box5 = new Box(810,150,70,70);
-Log3 = new Log(760,110,150,PI/6);
-Log4 = new Log(870,110,150,-PI/6);
-bird = new Bird(100,100)
+function preload (){
+    backgroundImage=loadImage("sprites/bg.png")
 }
 
-function draw() {
-  background("yellow");
+function setup(){
+    var canvas = createCanvas(1200,400);
+    engine = Engine.create();
+    world = engine.world;
 
-  Engine.update(gameengine)
+    
+    ground = new Ground(600,height,1200,20)
 
-  box1.display();
-  box2.display();
-  box3.display();
-  box4.display();
-  box5.display();
-  ground.display();
-  Pig1.display();
-  Pig2.display();
-  Log1.display();
-  Log2.display();
-  Log3.display();
-  Log4.display();
-  bird.display();
+    box1 = new Box(700,320,70,70);
+    box2 = new Box(920,320,70,70);
+    pig1 = new Pig(810, 350);
+    log1 = new Log(810,260,300, PI/2);
+
+    box3 = new Box(700,240,70,70);
+    box4 = new Box(920,240,70,70);
+    pig3 = new Pig(810, 220);
+
+    log3 =  new Log(810,180,300, PI/2);
+
+    box5 = new Box(810,160,70,70);
+    log4 = new Log(760,120,150, PI/7);
+    log5 = new Log(870,120,150, -PI/7);
+
+    bird = new Bird(100,100);
+
+}
+
+function draw(){
+    background(backgroundImage);
+    Engine.update(engine);
+    console.log(box2.body.position.x);
+    console.log(box2.body.position.y);
+    console.log(box2.body.angle);
+    box1.display();
+    box2.display();
+    ground.display();
+    pig1.display();
+    log1.display();
+
+    box3.display();
+    box4.display();
+    pig3.display();
+    log3.display();
+
+    box5.display();
+    log4.display();
+    log5.display();
+
+    bird.display();
 }
